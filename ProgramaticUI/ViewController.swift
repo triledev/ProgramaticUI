14ad67602e17883de4a23d3fb181be106e59198b
Tri Le <tri.le.mail@gmail.com>
2021-10-13 21:22:30 -0400

    Demonstration Collection Views using Programatic UI w/out Storyboard.

--- ProgramaticUI/ViewController.swift
+++ ProgramaticUI/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CustomCollectionViewController: UICollectionViewController {
+    
+    let customCellIdentifier = "customCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.backgroundColor = UIColor.blue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: customCellIdentifier)
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath) as UICollectionViewCell
+        cell.backgroundColor = self.randomColor()
+        
+        return cell
+    }
+    
+    func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
-
